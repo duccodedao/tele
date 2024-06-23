e52a663e60d27fa375f84127eb8d766f553ff4ec
Duc_hoc_code <100946720+duccodedao@users.noreply.github.com>
2024-06-23 15:08:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
         // Hàm lấy dữ liệu từ CoinGecko API
         function fetchCryptoData() {
-            var url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1000&page=1&sparkline=false';
+            var url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=300&page=1&sparkline=false';
 
             return fetch(url)
                 .then(response => {
