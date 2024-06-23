e929d75e2ad6f3a9a1bf847f591ff55ce2ad1442
Duc_hoc_code <100946720+duccodedao@users.noreply.github.com>
2024-06-23 16:02:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -36,57 +36,38 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 
-        // Hàm lấy dữ liệu từ CoinGecko API
-        function fetchCryptoData() {
-            var url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=300&page=1&sparkline=false';
+   // Hàm lấy dữ liệu từ Binance API
+function fetchCryptoData() {
+    var url = 'https://api.binance.com/api/v3/ticker/price';
 
-            return fetch(url)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .catch(error => {
-                    console.error('Đã xảy ra lỗi:', error);
-                });
-        }
-
-        // Hàm hiển thị dữ liệu tỉ giá thị trường crypto
-        function displayCryptoData(data) {
-            var cryptoPricesElement = document.getElementById('cryptoPrices');
-            cryptoPricesElement.innerHTML = ''; // Xóa nội dung cũ trước khi thêm dữ liệu mới
-
-            for (const coin of data) {
-                var cryptoName = coin.name;
-                var cryptoPrice = coin.current_price.toFixed(2);
-                var listItem = document.createElement('li');
-                listItem.classList.add('crypto-item');
-                listItem.innerHTML = `<span class="crypto-name">${cryptoName}</span>: <span class="crypto-price">$${cryptoPrice}</span>`;
-                cryptoPricesElement.appendChild(listItem);
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
             }
-        }
+            return response.json();
+        })
+        .catch(error => {
+            console.error('Đã xảy ra lỗi:', error);
+        });
+}
 
-        // Hàm cập nhật dữ liệu giá crypto
-        function updateCryptoPrices() {
-            fetchCryptoData()
-                .then(data => {
-                    displayCryptoData(data);
-                });
-        }
+// Hàm hiển thị dữ liệu tỉ giá thị trường crypto
+function displayCryptoData(data) {
+    var cryptoPricesElement = document.getElementById('cryptoPrices');
+    cryptoPricesElement.innerHTML = ''; // Xóa nội dung cũ trước khi thêm dữ liệu mới
 
-        // Cập nhật giá crypto khi trang web được load lại
-        updateCryptoPrices();
-        // Cập nhật giá crypto mỗi 5 phút
-        setInterval(updateCryptoPrices, 300000); // 300000 milliseconds = 5 minutes
+    for (const coin of data) {
+        var cryptoSymbol = coin.symbol;
+        var cryptoPrice = parseFloat(coin.price).toFixed(2);
+        var listItem = document.createElement('li');
+        listItem.classList.add('crypto-item');
+        listItem.innerHTML = `<span class="crypto-symbol">${cryptoSymbol}</span>: <span class="crypto-price">$${cryptoPrice}</span>`;
+        cryptoPricesElement.appendChild(listItem);
+    }
+}
 
-
-
-
-
-
-
-// Hàm cập nhật dữ liệu giá crypto và hiển thị thời gian cập nhật
+// Hàm cập nhật dữ liệu giá crypto
 function updateCryptoPrices() {
     fetchCryptoData()
         .then(data => {
@@ -101,12 +82,13 @@ function displayLastUpdatedTime() {
     var currentTime = new Date();
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
     var day = currentTime.getDate();
     var month = currentTime.getMonth() + 1; // Số tháng tính từ 0
     var year = currentTime.getFullYear();
 
     // Định dạng thời gian cập nhật
-    var updateTime = hours + ':' + (minutes < 10 ? '0' + minutes : minutes) + ' ' + day + '/' + month + '/' + year;
+    var updateTime = hours + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds) + ' ' + day + '/' + month + '/' + year;
     lastUpdatedElement.textContent = 'Dữ liệu được cập nhật lúc: ' + updateTime;
 }
 
